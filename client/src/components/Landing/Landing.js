578a0f8af3c3f0344eb00253af50efdc467d68f9
Deepakl07 <136842923+Deepakl07@users.noreply.github.com>
2023-12-22 23:42:05 +0530

    changes made

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -8,8 +8,8 @@ const Landing = () => {
         <div className="landing__container">
             <div className="landing__header__container">
                 <div className="landing__header">
-                    <h3 className="landing__header__discount">UP TO 15% DISCOUNT</h3>
-                    <h1 className="landing__header__main">Checkout The Best Fashion Style</h1>
+                    <h3 className="landing__header__discount">LIST IT ! FIND IT!</h3>
+                    <h1 className="landing__header__main">Discover the Easy Way to Find Your Lost Belongings</h1>
                     <Link to="/shop">
                         <Button variant='outlined' sx={[ {width: '190px', height: '50px', borderRadius: '20px' , fontWeight: '700', backgroundColor: 'none', borderColor: 'black', color: 'black' }, {'&:hover': {  backgroundColor: "black" , color: "#FFE26E", borderColor: 'black'}}]}>SHOP NOW</Button>
                     </Link>
