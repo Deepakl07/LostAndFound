963954fa8f7e715166cf0ca41829e69a544d2448
Deepakl07 <136842923+Deepakl07@users.noreply.github.com>
2023-12-23 08:29:15 +0530

    changes made

--- client/src/components/Nav/Nav-Links/NavLinks.js
+++ client/src/components/Nav/Nav-Links/NavLinks.js
@@ -7,10 +7,10 @@ const NavLinks = () => {
                 <div className="bottom__container">
                     <ul className="nav">
                         <li className='nav-link'><Link to="/">Home</Link></li> 
-                        <li className='nav-link'><Link to="/shop">Shop</Link> </li>
-                        <li className='nav-link'><Link to="/category/men">Men</Link></li> 
-                        <li className='nav-link'><Link to="/category/women">Women</Link></li> 
-                        <li className='nav-link'><Link to="/category/kids">Kids</Link></li>
+                        <li className='nav-link'><Link to="/shop">Lost</Link> </li>
+                        <li className='nav-link'><Link to="/category/men">Found</Link></li> 
+                        <li className='nav-link'><Link to="/category/women">About</Link></li> 
+                        <li className='nav-link'><Link to="/category/kids">Contact</Link></li>
                     </ul>
                 </div>
             </nav>
